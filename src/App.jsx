78431bdb8a37bc78fb9e391db7c1aae4bd237a0d
maike1983 <Maikevandartel@outlook.com>
2026-06-03 10:53:17 +0200

    t/m stap 4 knoppen vorige volgende toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import PokemonCard from "./components/PokemonCard/PokemonCard.jsx";
 function App() {
 
     const [pokemonList, setPokemonList] = useState([]);
+    const [offset, setOffset] = useState(0);
+    const [nextUrl, setNextUrl] = useState(null);
+    const [previousUrl, setPreviousUrl] = useState(null);
 
     async function fetchPokemonList() {
         try {
             // const dittoResult = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
             // const jigglypuffResult = await axios.get("https://pokeapi.co/api/v2/pokemon/jigglypuff");
-            const listResult = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0");
+            const listResult = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`);
             console.log(listResult.data.results);
             // const detailRequests = listResult.data.results.map((pokemon) => {
             //     return axios.get(pokemon.url);
@@ -29,6 +32,8 @@ function App() {
 
             console.log(fullPokemonList);
             setPokemonList(fullPokemonList);
+            setNextUrl(listResult.data.next);
+            setPreviousUrl(listResult.data.previous);
 
             // const twentyPokemonDetails = await axios.get(twentyPokemon);
             // console.log(twentyPokemonDetails.data)
@@ -40,11 +45,23 @@ function App() {
 
     useEffect(() => {
         fetchPokemonList();
-    }, []);
+    }, [offset]);
+
+    function handleNext() {
+        setOffset(offset + 20);
+    }
+
+    function handlePrevious() {
+        setOffset(offset - 20);
+    }
 
     return (
         <>
             <h1>Gotta catch em all!</h1>
+
+            <button type="button" onClick={handlePrevious} disabled={!previousUrl}>Vorige</button>
+            <button type="button" onClick={handleNext} disabled={!nextUrl}>Volgende</button>
+
             {pokemonList.map((pokemon) => (
                 <PokemonCard key={pokemon.id} currentPokemon={pokemon}/>
             ))}
