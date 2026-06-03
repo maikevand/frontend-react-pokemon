8d1d5ed472c6e4795dd88bfde351a494ff4af239
maike1983 <Maikevandartel@outlook.com>
2026-06-03 11:32:19 +0200

    t/m stap 6, error, loading en memory leak toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,17 @@ function App() {
     const [offset, setOffset] = useState(0);
     const [nextUrl, setNextUrl] = useState(null);
     const [previousUrl, setPreviousUrl] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
 
     async function fetchPokemonList() {
         try {
-            // const dittoResult = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
-            // const jigglypuffResult = await axios.get("https://pokeapi.co/api/v2/pokemon/jigglypuff");
+            setLoading(true);
+            setError("");
+
             const listResult = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`);
-            console.log(listResult.data.results);
-            // const detailRequests = listResult.data.results.map((pokemon) => {
-            //     return axios.get(pokemon.url);
-            // });
-            // setPokemonList([dittoResult.data, jigglypuffResult.data]);
-            // const firstPokemon = listResult.data.results[0];
+
             const twentyPokemon = listResult.data.results;
-            // console.log(firstPokemon);
             const fullPokemonList = [];
 
             for (const pokemon of twentyPokemon) {
@@ -30,16 +27,15 @@ function App() {
                 fullPokemonList.push(detailResult.data);
             }
 
-            console.log(fullPokemonList);
             setPokemonList(fullPokemonList);
             setNextUrl(listResult.data.next);
             setPreviousUrl(listResult.data.previous);
 
-            // const twentyPokemonDetails = await axios.get(twentyPokemon);
-            // console.log(twentyPokemonDetails.data)
-            // setPokemonList([twentyPokemonDetails.data]);
         } catch (e) {
             console.error(e);
+            setError("Er ging iets mis bij het ophalen van de Pokémon.");
+        } finally {
+            setLoading(false);
         }
     }
 
@@ -62,12 +58,13 @@ function App() {
             <button type="button" onClick={handlePrevious} disabled={!previousUrl}>Vorige</button>
             <button type="button" onClick={handleNext} disabled={!nextUrl}>Volgende</button>
 
+            {loading && <p>Pokémon worden geladen...</p>}
+
+            {error && <p>{error}</p>}
+
             {pokemonList.map((pokemon) => (
                 <PokemonCard key={pokemon.id} currentPokemon={pokemon}/>
             ))}
-            {/*{pokemonList.map((pokemon) => (*/}
-            {/*<p key={pokemon.name}>{pokemon.name}</p>*/}
-            {/*))}*/}
         </>
     );
 }
