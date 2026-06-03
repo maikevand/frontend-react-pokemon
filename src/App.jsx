4e7238e362f9e5f729dd5eae237169a11ea120b5
maike1983 <Maikevandartel@outlook.com>
2026-06-03 09:48:08 +0200

    Begin stap 3, 20 namen worden weergegeven

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,15 @@ function App() {
 
     async function fetchPokemonList() {
         try {
-            const dittoResult = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
-            const jigglypuffResult = await axios.get("https://pokeapi.co/api/v2/pokemon/jigglypuff");
-            setPokemonList([dittoResult.data, jigglypuffResult.data]);
+            // const dittoResult = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
+            // const jigglypuffResult = await axios.get("https://pokeapi.co/api/v2/pokemon/jigglypuff");
+            const listResult = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0");
+            console.log(listResult.data.results);
+            // const detailRequests = listResult.data.results.map((pokemon) => {
+            //     return axios.get(pokemon.url);
+            // });
+            // setPokemonList([dittoResult.data, jigglypuffResult.data]);
+            setPokemonList(listResult.data.results);
         } catch (e) {
             console.error(e);
         }
@@ -24,8 +30,11 @@ function App() {
     return (
         <>
             <h1>Gotta catch em all!</h1>
+            {/*{pokemonList.map((pokemon) => (*/}
+            {/*    <PokemonCard key={pokemon.id} currentPokemon={pokemon}/>*/}
+            {/*))}*/}
             {pokemonList.map((pokemon) => (
-                <PokemonCard key={pokemon.id} currentPokemon={pokemon}/>
+            <p key={pokemon.name}>{pokemon.name}</p>
             ))}
         </>
     );
